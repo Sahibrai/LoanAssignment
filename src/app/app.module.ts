66055cb4c9bafe089ec236acc51551a9d14ff7a3
Sahib Rai <sahib0246.cse19@chitkara.edu.in>
2022-06-09 15:58:37 +0530

    loan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { LoanEligibilityComponent } from './loan-eligibility/loan-eligibility.component';
+const routes: Routes = [
+  
+  {path:"LoanEligibility",component:LoanEligibilityComponent},
+  {path:"",component:HomeComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoanEligibilityComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
